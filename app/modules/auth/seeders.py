4f774da3d4bd0631c67a774f5fbfc4e1f4443a22
macpercam15 <macpercam@aalum.us.es>
2026-01-15 15:33:32 +0100

    MINOR CHANGES

--- app/modules/auth/seeders.py
+++ app/modules/auth/seeders.py
@@ -13,6 +13,8 @@ class AuthSeeder(BaseSeeder):
         users = [
             User(email="user1@example.com", password="1234"),
             User(email="user2@example.com", password="1234"),
+            User(email="user3@example.com", password="1234"),
+
         ]
 
         # Inserted users with their assigned IDs are returned by `self.seed`.
